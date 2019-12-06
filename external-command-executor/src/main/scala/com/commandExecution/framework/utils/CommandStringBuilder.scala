8cb314f1dafd0f56c63b8ce0e915314b9ad548c4
Aditya Gupta <agupt24@uic.edu>
2019-12-06 14:33:52 -0600

    AG: Graded, feedback added

--- external-command-executor/src/main/scala/com/commandExecution/framework/utils/CommandStringBuilder.scala
+++
@@ -1,7 +0,0 @@
-package com.commandExecution.framework.utils
-
-object CommandStringBuilder {
-
-  def getGetStringFromCommandStrings(commandStrings: Seq[String]): String = commandStrings.foldLeft("")((a, b) => a.concat(b).concat(" ")).trim
-
-}
